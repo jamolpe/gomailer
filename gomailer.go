f1be76b61b758e9a7901baaac97da5b7fb2bb579
jamolpe <jamolpe@gmail.com>
2020-08-01 18:55:28 +0200

    feat: validation

--- gomailer.go
+++ gomailer.go
@@ -7,12 +7,17 @@ import (
 )
 
 type IMailer interface {
-	SendPlainEmail(data models.Email) (bool, error)
-	SendTemplateMail(data models.Email, tmplt *template.Template, templateData interface{}) (bool, error)
+	SendPlainEmail(data models.Email) (bool, *ValidationError, error)
+	SendTemplateMail(data models.Email, tmplt *template.Template, templateData interface{}) (bool, *ValidationError, error)
 }
 
 type Mailer struct {
 	core core.ICore
+}
+
+type ValidationError struct {
+	Code    string
+	Message string
 }
 
 func New(config models.Configuration) IMailer {
@@ -21,18 +26,37 @@ func New(config models.Configuration) IMailer {
 	}
 }
 
-func (m *Mailer) SendPlainEmail(data models.Email) (bool, error) {
-	worked, err := m.core.SendMail(data, true)
-	if worked {
-		return worked, nil
+func (m *Mailer) validateMinimumData(data models.Email) *ValidationError {
+	if data.Body == "" {
+		return &ValidationError{Code: "MAILER_VALIDATION_ERROR001", Message: "email body con not be void"}
 	}
-	return false, err
+	if data.From == "" {
+		return &ValidationError{Code: "MAILER_VALIDATION_ERROR002", Message: "email from can not be void"}
+	}
+	if !(len(data.To) > 0) {
+		return &ValidationError{Code: "MAILER_VALIDATION_ERROR003", Message: "email has no from to send"}
+	}
+	return nil
 }
 
-func (m *Mailer) SendTemplateMail(data models.Email, tmplt *template.Template, templateData interface{}) (bool, error) {
+func (m *Mailer) SendPlainEmail(data models.Email) (bool, *ValidationError, error) {
+	if validation := m.validateMinimumData(data); validation != nil {
+		return false, validation, nil
+	}
+	worked, err := m.core.SendMail(data, true)
+	if worked {
+		return worked, nil, nil
+	}
+	return false, nil, err
+}
+
+func (m *Mailer) SendTemplateMail(data models.Email, tmplt *template.Template, templateData interface{}) (bool, *ValidationError, error) {
+	if validation := m.validateMinimumData(data); validation != nil {
+		return false, validation, nil
+	}
 	worked, err := m.core.SendHTMLMail(data, tmplt, templateData)
 	if worked {
-		return worked, nil
+		return worked, nil, nil
 	}
-	return false, err
+	return false, nil, err
 }
